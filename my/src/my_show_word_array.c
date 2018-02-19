5d65e70731d0bafbe1947d00979505cba0b5d007
Cyril COLINET <cyril.colinet@epitech.eu>
2018-02-19 14:41:59 +0100

    :wrench: Change to static library compilation

--- my/src/my_show_word_array.c
+++ my/src/my_show_word_array.c
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2017
 ** my_show_word_array
 ** File description:
-** Function that displays the content of an array of words. There must be one word per line, and each word must end with carriage return, including the last one
+** Display array content
 */
 
 # include "my.h"
@@ -10,8 +10,9 @@
 void my_show_word_array(char * const *tab)
 {
 	int i = 0;
-	while (tab[i] != 0) {
-		if (my_str_isalpha(tab[i]) && tab[i][0] != '\0') {
+
+	while (tab[i]) {
+		if (my_str_isalpha(tab[i]) && tab[i][0]) {
 			my_putstr(tab[i]);
 			my_putchar('\n');
 			i++;
